e18f4e2e0b4a2f6bcaa84be0028fec25dda8d669
sylvain charpiot <sylvain@drawn.fr>
2019-02-13 17:11:30 +0100

    exercice terminé raké avec formulaire sur page detail restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,21 @@
 class ReviewsController < ApplicationController
+  before_action :find_review, only: [ :new, :create ]
+
   def index
     @reviews = Review.all
   end
 
   def new
     @review = Review.new
-    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
     @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
@@ -25,4 +25,7 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 
+  def find_review
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'reviews/new'
   get 'reviews/create'
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   resources :reviews, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
